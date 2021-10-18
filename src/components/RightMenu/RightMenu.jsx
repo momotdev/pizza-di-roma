edf64888ab8b5cbac97030e61484eaad0b462950
Dmytro Momot <momotdev@gmail.com>
2021-10-18 14:31:07 +0300

    change method to tracking active menu link

--- src/components/RightMenu/RightMenu.jsx
+++ src/components/RightMenu/RightMenu.jsx
@@ -5,30 +5,27 @@ import usersIcon from '../../images/user.png';
 import productsIcon from '../../images/products.png';
 import ordersIcon from '../../images/box.png';
 import settingsIcon from '../../images/control.png';
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 import {useLocation} from "react-router";
 
 const RightMenu = () => {
 	const menuItems = [
-		{icon: dashboardIcon, label: 'Dashboard', link: ''},
+		{icon: dashboardIcon, label: 'Dashboard', link: '/dashboard'},
 		{icon: usersIcon, label: 'Users', link: '/users'},
 		{icon: productsIcon, label: 'Products', link: '/products'},
-		{icon: ordersIcon, label: 'Orders', link: ''},
-		{icon: settingsIcon, label: 'Settings', link: ''},
+		{icon: ordersIcon, label: 'Orders', link: '/orders'},
+		{icon: settingsIcon, label: 'Settings', link: '/settings'},
 	]
-
-	const currentPath = useLocation().pathname;
-	const activeStyle = [classes['menu-item'], classes['menu-item--active']];
 
 	return (
 		<div className={classes.menu}>
 			{menuItems.map(item => (
-				<Link key={item.label} to={item.link}>
-					<div className={currentPath === item.link ? activeStyle.join(' ') : classes['menu-item']}>
+				<NavLink key={item.label} to={item.link} activeClassName={classes['menu-item--active']}>
+					<div className={classes['menu-item']}>
 						<img src={item.icon} className={classes.icon} alt={item.label}/>
 						<div className={classes.label}>{item.label}</div>
 					</div>
-				</Link>
+				</NavLink>
 			))}
 		</div>
 	);
